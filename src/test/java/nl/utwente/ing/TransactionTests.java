62a367ea80c46e1923298861015369740dc69ce9
joostprins <joost_prins@hotmail.com>
2018-03-17 18:38:20 +0100

    Add all tests for the /transactions GET endpoint

--- src/test/java/nl/utwente/ing/TransactionTests.java
+++ src/test/java/nl/utwente/ing/TransactionTests.java
@@ -29,43 +29,49 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.nio.file.Path;
+import java.net.URI;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TransactionTests {
 
-    private static final Path TRANSACTION_LIST_SCHEMA_PATH = Paths.get("src/test/java/nl/utwente/ing/schemas" +
-                    "/transactions/transaction-list.json");
-    private static final Path TRANSACTION_SCHEMA_PATH = Paths.get("src/test/java/nl/utwente/ing/schemas" +
-                    "/transactions/transaction.json");
+    private static final URI TRANSACTION_LIST_SCHEMA_PATH = Paths.get("src/test/java/nl/utwente/ing/schemas" +
+                    "/transactions/transaction-list.json").toAbsolutePath().toUri();
+    private static final URI TRANSACTION_SCHEMA_PATH = Paths.get("src/test/java/nl/utwente/ing/schemas" +
+                    "/transactions/transaction.json").toAbsolutePath().toUri();
 
     private static final int TEST_TRANSACTION_ID_1 = 68796973;
     private static final int TEST_TRANSACTION_ID_2 = 23890471;
+
+    private static final String TEST_CATEGORY_1 = "{\"id\": 0, \"name\": \"string\"}";
+    private static final String TEST_CATEGORY_2 = "{\"id\": 1, \"name\": \"work\"}";
+
     private static final String TEST_TRANSACTION_1 = String.format("{\"id\": %d, " +
                     "\"date\": \"1889-04-20T19:45:04.030Z\", " +
                     "\"amount\": 0, " +
                     "\"external-iban\": \"string\", " +
                     "\"type\": \"deposit\", " +
-                    "\"category\": {\"id\": 0, \"name\": \"string\"}}",
+                    "\"category\": " + TEST_CATEGORY_1 + "}",
                     TEST_TRANSACTION_ID_1);
     private static final String TEST_TRANSACTION_2 = String.format("{\"id\": %d, " +
                     "\"date\": \"1889-04-20T19:45:04.030Z\", " +
                     "\"amount\": 10, " +
                     "\"external-iban\": \"strings\", " +
                     "\"type\": \"withdrawal\", " +
-                    "\"category\": {\"id\": 0, \"name\": \"string\"}}",
+                    "\"category\": " + TEST_CATEGORY_2 + "}",
             TEST_TRANSACTION_ID_2);
     private static final String TEST_TRANSACTION_INVALID = String.format("{\"id\": %d, " +
                     "\"data\": \"1889-04-20T19:45:04.030Z\", " +
-                    "\"category\": {\"id\": 0, \"name\": \"string\"}}",
+                    "\"category\": " + TEST_CATEGORY_1 + "}",
             TEST_TRANSACTION_ID_1);
+
+    private static final int TEST_OFFSET_NUMBER = 1;
 
     private static Integer sessionId;
 
@@ -105,7 +111,6 @@ public class TransactionTests {
                 .statusCode(204);
     }
 
-    //TODO: Create test cases for GET /transactions offset, limit, category
     /*
      *  Tests related to GET requests on the /transactions API endpoint.
      *  API Documentation: https://app.swaggerhub.com/apis/djhuistra/INGHonours/1.0.1#/transactions
@@ -123,7 +128,7 @@ public class TransactionTests {
                 .get("api/v1/transactions")
                 .then()
                 .assertThat()
-                .body(matchesJsonSchema(TRANSACTION_LIST_SCHEMA_PATH.toAbsolutePath().toUri()))
+                .body(matchesJsonSchema(TRANSACTION_LIST_SCHEMA_PATH))
                 .contentType(ContentType.JSON)
                 .statusCode(200)
                 .extract()
@@ -132,6 +137,96 @@ public class TransactionTests {
                 .size();
 
         assertThat(size, lessThanOrEqualTo(20));
+    }
+
+    /**
+     * Performs a GET request on the transactions endpoint with a specified offset.
+     *
+     * This test uses a valid session ID and tests whether the server handles the offset parameter correctly.
+     */
+    @Test
+    public void validSessionTransactionsGetOffsetTest() {
+        // Insert test transaction 1.
+        validSessionValidTransactionPostTest();
+
+        // Insert test transaction 2.
+        given()
+                .header("X-session-ID", sessionId)
+                .body(TEST_TRANSACTION_2)
+                .post("api/v1/transactions");
+
+        // Send a valid get request with offset 1.
+        String response = given()
+                .header("X-session-ID", sessionId)
+                .queryParam("offset", 1)
+                .get("api/v1/transactions")
+                .then()
+                .assertThat()
+                .body(matchesJsonSchema(TRANSACTION_LIST_SCHEMA_PATH))
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .extract()
+                .response()
+                .body()
+                .path("[0]");
+
+        assertThat(response, equalTo(TEST_TRANSACTION_2));
+    }
+
+    /**
+     * Performs a GET request on the transactions endpoint with a specified category.
+     *
+     * This test uses a valid session ID and tests whether the server handles the category parameter correctly.
+     */
+    @Test
+    public void validSessionTransactionsGetCategoryTest() {
+        // Insert two different transactions with two different categories into the API.
+        validSessionTransactionsGetOffsetTest();
+
+        Object[] categories = given()
+                .header("X-session-ID", sessionId)
+                .queryParam("category", "work")
+                .get("/api/v1/transactions")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(matchesJsonSchema(TRANSACTION_LIST_SCHEMA_PATH))
+                .contentType(ContentType.JSON)
+                .extract()
+                .jsonPath()
+                .getList("$[*].category.name")
+                .toArray();
+
+        for (Object category : categories) {
+            assertThat(category, equalTo("work"));
+        }
+    }
+
+    /**
+     * Performs a GET request on the transactions endpoint with a specified limit.
+     *
+     * This test uses a valid session ID and tests whether the server handles the category parameter correctly.
+     */
+    @Test
+    public void validSessionTransactionsGetLimitTest() {
+        // Insert two transactions into the API.
+        validSessionTransactionsGetOffsetTest();
+
+        int size = given()
+                .header("X-session-ID", sessionId)
+                .queryParam("limit", TEST_OFFSET_NUMBER)
+                .get("/api/v1/transactions")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(matchesJsonSchema(TRANSACTION_LIST_SCHEMA_PATH))
+                .contentType(ContentType.JSON)
+                .extract()
+                .jsonPath()
+                .getList("$")
+                .size();
+
+        assertThat(TEST_OFFSET_NUMBER, equalTo(size));
     }
 
     /**
@@ -168,14 +263,14 @@ public class TransactionTests {
                 .get(String.format("api/v1/transactions/%d", TEST_TRANSACTION_ID_1))
                 .then()
                 .assertThat()
-                .body(matchesJsonSchema(TRANSACTION_SCHEMA_PATH.toAbsolutePath().toUri()))
+                .body(matchesJsonSchema(TRANSACTION_SCHEMA_PATH))
                 .contentType(ContentType.JSON)
                 .statusCode(200)
                 .extract()
                 .response()
                 .asString();
 
-        assertEquals(transaction, TEST_TRANSACTION_1);
+        assertThat(TEST_TRANSACTION_1, equalTo(transaction));
     }
 
     /*
